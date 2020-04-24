babee6452f4e042c73b457d14b4560a044603b84
RalphLincoln <ialaneme@yahoo.com>
2020-04-24 01:16:28 +0100

    Styled my todoInput Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import uuid from 'uuid';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoInput from './Components/TodoInput'
+import uuid from 'react-uuid'
+import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
 
+
 // Testing my vs code git hub
-function App() {
-  return (
-    <div className="App">
-      <TodoInput />
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [{ id: 1, title: 'wake up' }, { id: 2, title: 'make breakfast' }],
+      id: uuid(),
+      item: '',
+      editItem: false
+    }
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">
+              Todo input
+            </h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            editItem: false,
+            item: ''
+        }
+    }
+
+    handleChange = () => {
+        console.log('Handle Change')
+    }
+
+    handleSubmit = () => {
+        console.log('Handle submit')
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello from the todoList input</h1>
+                <div className="card border-primary card-body my-3">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text bg-primary text-white">
+                                    <i className="fas fa-book"></i>
+                                </div>
+                            </div>
+                            <input type="text" className='form-control  text-capitalize' placeholder='Add todo item' value={this.state.item} onChange={this.handleChange} />
+                        </div>
+                        <button className="btn btn-outline-primary btn-block mt-3 text-uppercase">add item</button>
+                    </form>
+                </div>
             </div>
         )
     }
